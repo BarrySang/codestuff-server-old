9fd2e4a5f295aebb93e6a0433ce39d7673300169
BarrySang <barrysang8@gmail.com>
2021-07-04 15:16:46 +0300

    check if username is already in use before registering a user

--- routes/auth.js
+++ routes/auth.js
@@ -16,6 +16,12 @@ router.post('/signup', (req, res) => {
         return usedEmail;
     }
 
+    //function to check if username already exists
+    async function getUsedUname() {
+        let usedUname = await User.findOne({where: {username: username}});
+        return usedUname;
+    }
+
     //check required fields
     if(!first_name || !other_name || !email  || !username || !password || !passwordconfirm ) {
         errors.push({ msg: 'Ensure all fields are filled'});
@@ -42,28 +48,34 @@ router.post('/signup', (req, res) => {
         if(data !== null) {
             res.status(400).json('email already in use');
         } else {
-            if(data === null) {
-                //Hash password and register user
-                bcrypt.genSalt(10, (err, salt) => bcrypt.hash(password, salt, (err, hash) => {
-                    if(err) throw err;
-                    hashed_password = hash;
+            getUsedUname().then(data => {
+                if(data !== null) {
+                    res.status(400).json({error: 'username already in use'});
+                } else {
+                    //Hash password and register user
+                    bcrypt.genSalt(10, (err, salt) => bcrypt.hash(password, salt, (err, hash) => {
+                        if(err) throw err;
+                        hashed_password = hash;
 
-                    User.create({
-                        first_name,
-                        other_name,
-                        email,
-                        username,
-                        hashed_password
-                    })
-                    .then(user => {
-                        res.status(200).json({success: 'registration succesful'});
-                    })
-                    .catch(err => {
-                        console.log(err);
-                        res.status(400).json({error: 'an error occured'});
-                    });
-                }));
-            }
+                        User.create({
+                            first_name,
+                            other_name,
+                            email,
+                            username,
+                            hashed_password
+                        })
+                        .then(user => {
+                            res.status(200).json({success: 'registration succesful'});
+                        })
+                        .catch(err => {
+                            console.log(err);
+                            res.status(400).json({error: 'an error occured'});
+                        });
+                    }));
+                }
+            }).catch(err => {
+                console.log(err);
+            });
         }
     }).catch(err => console.log(err));
 
